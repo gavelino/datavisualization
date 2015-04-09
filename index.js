805348844987312b1e00455fce14a15e0e39f6e2
hsborges <hudsonsilbor@gmail.com>
2015-04-09 18:20:56 -0300

    Adicionei comentarios no codigo para facilitar a compreensão

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// Variavel que armazena os dados do problema
+// É um mapa que contém os grafos e as distancias entre todos vertices de todos os grafos
 var appData;
 
+// Função que carrega o menu à esquerda (lista de grafos)
 function loadMenu() {
     $.each(appData.graphs, function(i, object){
         jQuery('<a/>', {
@@ -13,19 +16,28 @@ function loadMenu() {
     });
 }; 
 
+// Função que trata a mudança de grafos (clique em um grafo da lista lateral)
 function refreshGraph(i) {
+    // Escode o grafo e apresenta um gif loading
     $("#graphs-area").hide();
     $("#legend-area").hide();
     $("#loading").show();
+    
+    // Chama a função que realmente desenha o grafo e a legenda
     drawGraph(appData.graphs[i], i);
     drawLegend(color);
+    
+    // Ativa (deixa azul) o grafo selecionado na lista lateral
     $(".graph_menu.active").removeClass("active");
     $("a.graph_menu[graphId="+i+"]").addClass("active");
+    
+    // Mostra o grafo desenhado
     $("#loading").hide();
     $("#graphs-area").show();
     $("#legend-area").show();
 };
 
+// Função auxiliar que define as cores para as categorias
 function configureColors() {
     var labels = [];
     for(var c = 'A'.charCodeAt(0); c <= 'Z'.charCodeAt(0); c++ ) {
@@ -35,20 +47,26 @@ function configureColors() {
     color.domain(labels);
 }
 
+
+// Raio de uma "rosquinha"
 var radius = 74,
     padding = 10; 
 
+// Intervalo de cores disponiveis
 var color = d3.scale.ordinal()
     .range(["#393b79","#5254a3","#6b6ecf","#9c9ede","#637939","#8ca252","#b5cf6b","#cedb9c","#8c6d31","#bd9e39","#e7ba52","#e7cb94","#843c39","#ad494a","#d6616b","#e7969c","#7b4173","#a55194","#ce6dbd","#de9ed6","#1f77b4	","#aec7e8	","#ff7f0e	","#ffbb78	","#2ca02c	","#98df8a	","#d62728	","#ff9896	","#9467bd	","#c5b0d5	","#8c564b	","#c49c94	","#e377c2	","#f7b6d2	","#7f7f7f	","#c7c7c7	","#bcbd22	","#dbdb8d	","#17becf	","#9edae5"]);
 
+// ....
 var arc = d3.svg.arc()
     .outerRadius(radius)
     .innerRadius(radius - 30);
 
+// ....
 var pie = d3.layout.pie()
     .sort(null)
     .value(function(d) { return d.count; });
 
+// Função que desenha um dado grafo na area reservada do html
 var drawGraph = function(graph, i){
     $("#graphs-area-content").empty();
     
@@ -90,6 +108,7 @@ var drawGraph = function(graph, i){
     });
 };
 
+// Função que desenha a legenda na area reservada do html
 var drawLegend = function(color){
     $("#legend-area-content").empty();
     
@@ -115,6 +134,8 @@ var drawLegend = function(color){
         .text(function(d) { return d; });
 };
 
+
+// Executado no carregamento da pagina, para preencher com o grafo 0
 $.getJSON("data.json", function(data){
     appData = data;
     loadMenu();
